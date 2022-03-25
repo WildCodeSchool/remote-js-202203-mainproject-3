4bde4b709cd2c8c9d55791bac8002618f710af14
chenuetLoic <loicchenuet@hotmail.fr>
2022-03-25 14:10:51 +0100

    switch

--- src/components/AnswersList.js
+++ src/components/AnswersList.js
@@ -6,16 +6,52 @@ function AnswersList({ answers, quizzList, counter }) {
   console.log(quizzList);
 
   const checkboxOrRadio = (quizzList[counter].multiple_correct_answers === 'true') ? 'checkbox' : 'radio';
+  
+  const [check, setCheck] = React.useState('');
+  const [answerA, setAnswerA] = React.useState('false');
+  const [answerB, setAnswerB] = React.useState('false');
+  const [answerC, setAnswerC] = React.useState('false');
+  const [answerD, setAnswerD] = React.useState('false');
+  const [answerE, setAnswerE] = React.useState('false');
+  const [answerF, setAnswerF] = React.useState('false');
 
-  const [answerA, setAnswerA] = React.useState(false);
-  const [answerB, setAnswerB] = React.useState(false);
-  const [answerC, setAnswerC] = React.useState(false);
-  const [answerD, setAnswerD] = React.useState(false);
-  const [answerE, setAnswerE] = React.useState(false);
-  const [answerF, setAnswerF] = React.useState(false);
-
+  
   function controle(event){
     event.preventDefault();
+    setCheck(event.target.value);
+    console.log(check);
+    console.log(check.target.id);
+    console.log(check.target.value);
+
+    // if (check.target.id === 'answer_a'){
+    //   setAnswerA('true');
+    // }
+
+    // Réinitialisation des states à false 
+    setAnswerA('false');
+    setAnswerB('false');
+    setAnswerC('false');
+    setAnswerD('false');
+    setAnswerE('false');
+    setAnswerF('false');
+
+
+    switch(check.target.id){
+      case 'answer_a' : setAnswerA('true');
+        break;
+      case 'answer_b' : setAnswerB('true');
+        break;
+      case 'answer_c' : setAnswerC('true');
+        break;
+      case 'answer_d' : setAnswerD('true');
+        break;
+      case 'answer_e' : setAnswerE('true');
+        break;
+      case 'answer_f' : setAnswerF('true');
+        break;
+    }
+
+
     console.log('A user:' + answerA);
     console.log('A object:' + quizzList[counter].correct_answers.answer_a_correct);
     console.log('B user:' + answerB);
@@ -59,41 +95,41 @@ function AnswersList({ answers, quizzList, counter }) {
         </div>
 
         {/* Affichage des réponses */}
-        <form className="reponse" type='submit' method='get' action='/'>
+        <form className="reponse" type='submit' method='get' action='/' onChange={setCheck.bind()}>
           <div className="listResponse">
-            <input type={checkboxOrRadio} name="answers" id='answer_a' onChange={event => setAnswerA(event.target.checked)} />
+            <input type={checkboxOrRadio} name="answers" id='answer_a' value='true' />
             <label htmlFor="answer_a" >{answers.answer_a}</label>
           </div>
 
           <div className="listResponse">
-            <input type={checkboxOrRadio} name="answers" id='answer_b' onChange={event => setAnswerB(event.target.checked)} />
+            <input type={checkboxOrRadio} name="answers" id='answer_b' value='true' />
             <label htmlFor="answer_b" >{answers.answer_b}</label>
           </div>
 
           {answers.answer_c ?
           <div className="listResponse">
-            <input type={checkboxOrRadio} name="answers" id='answer_c'onChange={event => setAnswerC(event.target.checked)} />
+            <input type={checkboxOrRadio} name="answers" id='answer_c' value='true' />
             <label htmlFor="answer_c" >{answers.answer_c}</label>
           </div>: null}
 
           {answers.answer_d ?
           <div className="listResponse">
-            <input type={checkboxOrRadio} name="answers" id='answer_d' onChange={event => setAnswerD(event.target.checked)} />
+            <input type={checkboxOrRadio} name="answers" id='answer_d' value='true' />
             <label htmlFor="answer_d" >{answers.answer_d}</label>
           </div>: null}
 
           {answers.answer_e ?
             <div className="listResponse">
-              <input type={ checkboxOrRadio } name="answers" id='answer_e' onChange={event => setAnswerE(event.target.checked)} />
+              <input type={checkboxOrRadio} name="answers" id='answer_e' value='true' />
               <label htmlFor="answer_e" >{answers.answer_e}</label>
             </div>: null}
           
           {answers.answer_f ?
             <div className="listResponse">
-              <input type={checkboxOrRadio} name="answers" id='answer_f' onChange={event => setAnswerF(event.target.checked)} />
+              <input type={checkboxOrRadio} name="answers" id='answer_f' value='true' />
               <label htmlFor="answer_f">{answers.answer_f}</label>
             </div>: null}
-            <button className="buttonHandleNext" onClick={() => controle(event)}>Validate</button>
+            <button className="buttonHandleValidate" onClick={() => controle(event)}>Validate</button>
       </form>
     </section>
   );
