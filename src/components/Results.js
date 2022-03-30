9fabe56da61001591523334b0cfcf6e5cbae0534
Piquenet <clement.piquenet@gmail.com>
2022-03-30 10:41:55 +0200

    add resultComment

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ResultComment from './ResultComment';
 
 function Results({resultCounter}){
 
     return(
         <div>
             <h2 className="felicitation">Vous avez un score de {resultCounter} sur 10</h2>
+            <ResultComment resultCounter={resultCounter}/>
             <button className = "buttonHome" type = "submit" onClick = {function () {window.location.reload();}}>Accueil</button>
         </div>
     );
