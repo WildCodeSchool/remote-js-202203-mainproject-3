78088b2b14445540228624ff76343ea7e23f3852
dthibaut1 <dthibaut1@free.fr>
2022-03-30 10:23:36 +0200

    update AnswersList QuizzList

--- src/components/AnswersList.js
+++ src/components/AnswersList.js
@@ -24,57 +24,14 @@ function AnswersList({ answers, quizzList, counter }) {
   
   // Fonction de controle des réponses
   function controle(event){
-    // Initialisation des variables
-    // let answerA= 'false';
-    // let answerB= 'false';
-    // let answerC= 'false';
-    // let answerD= 'false';
-    // let answerE= 'false';
-    // let answerF= 'false';
-    
+      
     event.preventDefault();
 
     // Disparaitre le bouton validate
 
     buttonValidateID.style.display = 'none';
 
-    // Affectation de la value 'true' suivant la réponse
-  //   setCheck(event.target.value);
-  //   switch(check.target.id){
-  //     case 'answer_a' : answerA = 'true';
-  //       break;
-  //     case 'answer_b' : answerB ='true';
-  //       break;
-  //     case 'answer_c' : answerC ='true';
-  //       break;
-  //     case 'answer_d' : answerD ='true';
-  //       break;
-  //     case 'answer_e' : answerE ='true';
-  //       break;
-  //     case 'answer_f' : answerF ='true';
-  //       break;
-  //   }
-    
-  //   //Stockage des réponses utilisateurs dans un tableau
-  //   const tabAnswersUser = [answerA, answerB, answerC, answerD, answerE, answerF];
-  //   //Stockage des réponses objet dans un tableau
-  //   const tabAnswers = [
-  //     quizzList[counter].correct_answers.answer_a_correct,
-  //     quizzList[counter].correct_answers.answer_b_correct,
-  //     quizzList[counter].correct_answers.answer_c_correct,
-  //     quizzList[counter].correct_answers.answer_d_correct,
-  //     quizzList[counter].correct_answers.answer_e_correct,
-  //     quizzList[counter].correct_answers.answer_f_correct];
-
-  //     // Comparaison des 2 tableaux
-  //     if ( JSON.stringify(tabAnswersUser) == JSON.stringify(tabAnswers)){
-  //       console.log('WIN');
-  //     }else{
-  //       console.log('LOSE');
-  //     }
-  // console.log(answers[0].checkAnswer());
-  // console.log(answers[1].checkAnswer());
-}
+  }
 
 
   return (
@@ -89,39 +46,6 @@ function AnswersList({ answers, quizzList, counter }) {
         {/* Affichage des réponses */}
         <form className="reponse" id="formCheck" type='submit' method='get' action='/' onChange={setCheck.bind()}>
           {answerRows}
-          {/* <div className="listResponse">
-            <input type={checkboxOrRadio} className='inputcheck' name="answers" id='answer_a' value='true'/>
-            <label htmlFor="answer_a" >{answers.answer_a}</label>
-          </div>
-
-          <div className="listResponse">
-            <input type={checkboxOrRadio} className='inputcheck' name="answers" id='answer_b' value='true' />
-            <label htmlFor="answer_b" >{answers.answer_b}</label>
-          </div>
-
-          {answers.length > 2 ?
-          <div className="listResponse">
-            <input type={checkboxOrRadio} className='inputcheck' name="answers" id='answer_c' value='true' />
-            <label htmlFor="answer_c" >{answers.answer_c}</label>
-          </div>: null}
-
-          {answers.length > 3 ?
-          <div className="listResponse">
-            <input type={checkboxOrRadio} className='inputcheck' name="answers" id='answer_d' value='true' />
-            <label htmlFor="answer_d" >{answers.answer_d}</label>
-          </div>: null}
-
-          {answers.length > 4 ?
-            <div className="listResponse">
-              <input type={checkboxOrRadio} className='inputcheck' name="answers" id='answer_e' value='true' />
-              <label htmlFor="answer_e" >{answers.answer_e}</label>
-            </div>: null}
-          
-          {answers.length > 5 ?
-            <div className="listResponse">
-              <input type={checkboxOrRadio} className='inputcheck' name="answers" id='answer_f' value='true' />
-              <label htmlFor="answer_f">{answers.answer_f}</label>
-            </div>: null} */}
           <button id="buttonHandleValidateID" className="buttonHandleValidate" onClick={() => controle(event)}>Validate</button> {/* */}
       </form>
     </section>
--- src/model/QuizzList.js
+++ src/model/QuizzList.js
@@ -7,20 +7,20 @@ import { Answer } from './Answer';
 //       this.newquizzlist = [];
 //   }
 
-export function  setNewQuizzList (quizzlist){
+export function setNewQuizzList(quizzlist) {
   const newquizzlist = [];
   const list_qa = ['answer_a', 'answer_b', 'answer_c', 'answer_d', 'answer_e', 'answer_f'];
   for (const quizz of quizzlist) {
-    const question = new Question(quizz.id, quizz.question, quizz.category, quizz.tags[0].name, 
-        quizz.difficulty, (quizz.multiple_correct_answers.toLowerCase() === 'true') ? true : false , quizz.correct_answer);
+    const question = new Question(quizz.id, quizz.question, quizz.category, quizz.tags[0].name,
+      quizz.difficulty, (quizz.multiple_correct_answers.toLowerCase() === 'true') ? true : false, quizz.correct_answer);
     const answers = quizz.answers;
     const c_answers = quizz.correct_answers;
     let correct_answer = [];
     let i = 0;
     while (i < list_qa.length) {
-      if (!answers[list_qa[i]]) {break; }
-      const answer = new Answer(list_qa[i], answers[list_qa[i]], 
-                    c_answers[list_qa[i] + '_correct'].toLowerCase() === 'true' ? true : false, false);
+      if (!answers[list_qa[i]]) { break; }
+      const answer = new Answer(list_qa[i], answers[list_qa[i]],
+        c_answers[list_qa[i] + '_correct'].toLowerCase() === 'true' ? true : false, false);
       question.answers.push(answer);
       if (c_answers[list_qa[i] + '_correct'] === 'true') {
         correct_answer.push(list_qa[i]);
@@ -28,9 +28,11 @@ export function  setNewQuizzList (quizzlist){
       i++;
     }
     if (question.multiple_correct_answers) {
-      question.correct_answer = correct_answer;}
+      question.correct_answer = correct_answer;
+    }
     if (!question.multiple_correct_answers && correct_answer.length > 0) {
-      question.correct_answer = correct_answer[0];}
+      question.correct_answer = correct_answer[0];
+    }
     newquizzlist.push(question);
   }
   return newquizzlist;
